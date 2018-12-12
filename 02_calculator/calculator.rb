4167173e74fde886b87f5880bb43833dca0ff67f
Matthew Saginario <matts@catprint.com>
2018-12-12 15:18:34 -0500

    02

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,24 @@
-#write your code here
+def add(a,b)
+ a + b 
+end 
+
+def subtract(a,b)
+  a - b
+end 
+
+def sum(array)
+ sum = 0
+  for num in array
+    sum += num
+  end
+  sum
+end 
+
+def multiply(array)
+ total = 1
+  for num in array
+   total *= num
+  end
+  total
+end 
+
