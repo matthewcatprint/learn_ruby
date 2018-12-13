13c325bf0aa67abf9146f236eb0d64bc2cbcf7b9
Matthew Saginario <matts@catprint.com>
2018-12-13 14:17:14 -0500

    03

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,35 @@
 #write your code here
+def echo(string)
+    if string == "bye"
+     'bye'
+    else 
+     'hello'
+    end
+end 
+
+def shout(string)
+    return string.upcase
+end 
+
+def repeat(text, n=2)
+    return [text] * n * ' '
+end
+
+def start_of_word(word, n=0)
+    word.split
+    return word [0,n] 
+end 
+
+def first_word(string)
+    return string.split.first
+end 
+
+def titleize(string)
+    array = string.split(" ")
+    except = ["and", "the", "over"]
+    array.each do |word|
+        word.capitalize! unless except.include?(word)
+    end 
+ array[0].capitalize! 
+ return array.join(" ")
+end 
