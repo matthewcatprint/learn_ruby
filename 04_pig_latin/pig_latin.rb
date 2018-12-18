34959479011cc972b4a3861d89d632a88f2c7dbf
Matthew Saginario <matts@catprint.com>
2018-12-18 13:59:05 -0500

    starting on 4

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,12 @@
-#write your code here
+def translate(str)
+  
+    letters = str.split(//)
+    @vowels= %w{a e i o u}
+      
+    if letters.any? { |x| @vowels.include?(x) }
+      @word = letters.map { |x| x}.push('ay').join('')
+      else
+          "oh shit"
+      end
+  end 
+  
