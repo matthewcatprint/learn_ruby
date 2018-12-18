b8e2680aa6cc51cb509921a009fa188109f0af0a
Matthew Saginario <matts@catprint.com>
2018-12-18 13:37:39 -0500

    5 done

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,29 +1,17 @@
 class Book
   attr_accessor :title
-
-  def initalize
-    @exceptions = %w[the a an and in]
-  end
-
+  
+ 
   def title=(str)
-    arr = str.split(" ")
-    if !arr.include?(@exceptions)
-      @title = arr.map(&:capitalize).join(' ')
-    else
-      "cool"
-    end
+    str.capitalize!
+    arr = str.split
+    @exceptions = %w{the a an and of in}
+    @title = arr.map {|word|
+      if @exceptions.include? (word)
+        word 
+     else 
+        word.capitalize
+      end}.join(' ')
   end
-
 
 end 
-
-def title=(new_title)
-  #words = new_title.split(' ').map(&:capitalize) 
-  if words.exclude?(@exceptions)
-    words = new_title.split(' ').map(&:capitalize) 
-  else #bad_words = words.include?(@exceptions)
-    bad_words.downcase
-  end
-  @title= words.join(' ') 
-end
-
