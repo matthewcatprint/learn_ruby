1765964c4d0cab3799a9932e3392d90d286bf82a
Matthew Saginario <matts@catprint.com>
2018-12-19 11:32:54 -0500

    ugh

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -2,11 +2,21 @@ def translate(str)
   
     letters = str.split(//)
     @vowels= %w{a e i o u}
-      
-    if letters.any? { |x| @vowels.include?(x) }
-      @word = letters.map { |x| x}.push('ay').join('')
-      else
+    @consonants = %w{b C D F G H J K L M N P Q R S T V W X Y Z}
+            
+        if letters.first == "a"
+            @word = letters.map { |x| x}
+        elsif letters.first(1) == "."
+            @word = letters.insert(-1, letters.delete_at(0))   
+         elsif letters.first(2) == "b" or "c" or "h"
+            @word = 
+          letters.insert(-1, letters.delete_at(0))
+          letters.insert(-1, letters.delete_at(0))
+        else 
           "oh shit"
-      end
-  end 
+        end
+          puts @word.push('ay').join('')
+    end 
+      
   
+
